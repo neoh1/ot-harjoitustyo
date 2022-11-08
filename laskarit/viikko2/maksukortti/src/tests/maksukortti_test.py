a28e50e70575b0ce8061e2b74deb73b6249bfa21
Niklas Harju <niklas.harju@helsinki.fi>
2022-11-08 13:44:29 +0200

    add probelm3 tests

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -22,9 +22,28 @@ class TestMaksukortti(unittest.TestCase):
         kortti.syo_edullisesti()
         self.assertEqual(str(kortti), "Kortilla on rahaa 2.00 euroa")
 
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(200)
+        kortti.syo_maukkaasti()
+        self.assertEqual(str(kortti), "Kortilla on rahaa 2.00 euroa")
+
+    def test_syo_edullisest_tasaraha(self):
+        kortti = Maksukortti(250)
+        kortti.syo_edullisesti()
+        self.assertEqual(str(kortti), "Kortilla on rahaa 0.00 euroa")
+
+    def test_syo_maukkaasti_tasaraha(self):
+        kortti = Maksukortti(400)
+        kortti.syo_maukkaasti()
+        self.assertEqual(str(kortti), "Kortilla on rahaa 0.00 euroa")
+
     def test_kortille_voi_ladata_rahaa(self):
         self.card.lataa_rahaa(2500)
         self.assertEqual(str(self.card), "Kortilla on rahaa 35.00 euroa")
+
+    def test_kortille_ei_ladata_neg_summa(self):
+        self.card.lataa_rahaa(-1000)
+        self.assertEqual(str(self.card), "Kortilla on rahaa 10.00 euroa")
 
     def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
         self.card.lataa_rahaa(20000)
