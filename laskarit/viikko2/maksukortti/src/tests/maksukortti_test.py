0e55d9636ed9a80aa4e678f50cfba65d3679c8da
Niklas Harju <niklas.harju@helsinki.fi>
2022-11-08 13:37:47 +0200

    init viikko2 tehtavat

---
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -0,0 +1,33 @@
+import unittest
+from maksukortti import Maksukortti
+
+class TestMaksukortti(unittest.TestCase):
+    def setUp(self):
+        self.card = Maksukortti(1000)
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        # card with 10 euros == 1000 cents
+        self.assertEqual(str(self.card), "Kortilla on rahaa 10.00 euroa")
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        self.card.syo_edullisesti()
+        self.assertEqual(str(self.card), "Kortilla on rahaa 7.50 euroa")
+
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+        self.card.syo_maukkaasti()
+        self.assertEqual(str(self.card), "Kortilla on rahaa 6.00 euroa")
+
+    def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(200)
+        kortti.syo_edullisesti()
+        self.assertEqual(str(kortti), "Kortilla on rahaa 2.00 euroa")
+
+    def test_kortille_voi_ladata_rahaa(self):
+        self.card.lataa_rahaa(2500)
+        self.assertEqual(str(self.card), "Kortilla on rahaa 35.00 euroa")
+
+    def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
+        self.card.lataa_rahaa(20000)
+        self.assertEqual(str(self.card), "Kortilla on rahaa 150.00 euroa")
+
+
