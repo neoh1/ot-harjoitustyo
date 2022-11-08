259e28a32ae95dc5cac76dcdede544fb60efd057
Niklas Harju <niklas.harju@helsinki.fi>
2022-11-08 14:01:28 +0200

    more tests, add coverage tool [teht6]

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,26 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataus_kasvattaa_saldoa(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+
+    def test_ota_rahaa_saldo_vahenee(self):
+        self.maksukortti.ota_rahaa(200)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 8.00 euroa")
+
+    def test_ota_rahaa_saldo_ei_muutu_jos_liian_vahan(self):
+        self.maksukortti.ota_rahaa(1100)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_ota_rahaa_bool(self):
+        testF = self.maksukortti.ota_rahaa(1100)
+        testT = self.maksukortti.ota_rahaa(800)
+        self.assertEqual(testF, False)
+        self.assertEqual(testT, True)
+
+
